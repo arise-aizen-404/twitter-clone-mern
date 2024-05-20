bf4e5188faff8b9dbb2437499a46d455190d230e
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-05-21 00:14:06 +0530

    setting up user routes

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,21 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { connectDB } from "./config/database.js";
+import { v2 as cloudinary } from "cloudinary";
 
 // Load environment variables from .env file
 dotenv.config({ path: ".env" });
-
-const port = process.env.PORT || 5005;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Create Express app
 const app = express();
+const port = process.env.PORT || 5005;
 
 // Middleware
 app.use(express.json());
@@ -30,3 +36,4 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
