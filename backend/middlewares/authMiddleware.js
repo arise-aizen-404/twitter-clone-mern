d81cffe7e2a4a8a749141f2752e01fc46160466a
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-05-20 21:49:35 +0530

    middleware for authentication

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler.js";
+import User from "../models/userModel.js";
+
+export const authenticate = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.jwt;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decodedJWT = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decodedJWT.userId).select("-password");
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized: token failed" });
+  }
+});
