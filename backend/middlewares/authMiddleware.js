a367da65c9b6bebc574330bcb282e376fbec25b8
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-05-21 11:21:46 +0530

    modified unauthoried access message

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,7 +4,8 @@ import User from "../models/userModel.js";
 
 export const authenticate = asyncHandler(async (req, res, next) => {
   let token = req.cookies.jwt;
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  if (!token)
+    return res.status(401).json({ message: "Unauthorized: No token" });
   try {
     const decodedJWT = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decodedJWT.userId).select("-password");
